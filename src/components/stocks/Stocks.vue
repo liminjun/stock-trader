b9802a1d1f2824ec049aa90c02bedede41877ade
liminjun <v-leeli@hotmail.com>
2020-08-02 11:10:30 +0800

    feat:creating stock components.

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,8 +1,25 @@
 <template>
-    <h1>Stocks component.</h1>
+    <div>
+        <app-stock v-for="stock in stocks" :key="$index">
+        </app-stock>
+    </div>
 </template>
 <script>
+import Stock from './Stock.vue'
 export default {
-    
+    data() {
+        return {
+            stocks: [
+                { id: 1, name: 'BMW', price: 10},
+                { id: 2, name: 'Google', price: 10},
+                { id: 1, name: 'Facebook', price: 10},
+                { id: 1, name: 'Apple', price: 10},
+                { id: 1, name: 'Tesla', price: 10}
+            ]
+        }
+    },
+    components: {
+        appStock: Stock
+    }
 }
 </script>
