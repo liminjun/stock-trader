76b1df6fda706be4a0abd5a503a22ff5158df3d2
liminjun <v-leeli@hotmail.com>
2020-08-02 12:28:53 +0800

    feat:adding buy button.

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <app-stock v-for="stock in stocks" :key="$index">
+        <app-stock v-for="stock in stocks" :stock='stock' :key="$index">
         </app-stock>
     </div>
 </template>
@@ -11,10 +11,10 @@ export default {
         return {
             stocks: [
                 { id: 1, name: 'BMW', price: 10},
-                { id: 2, name: 'Google', price: 10},
-                { id: 1, name: 'Facebook', price: 10},
-                { id: 1, name: 'Apple', price: 10},
-                { id: 1, name: 'Tesla', price: 10}
+                { id: 2, name: 'Google', price: 100},
+                { id: 3, name: 'Facebook', price: 20},
+                { id: 4, name: 'Apple', price: 200},
+                { id: 5, name: 'Tesla', price: 150}
             ]
         }
     },
