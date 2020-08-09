1721d4918ea98e46745ddf3e34bcbe0955c06aff
liminjun <v-leeli@hotmail.com>
2020-08-09 11:35:04 +0800

    feat:using html-webpack-plugin for output the index.html in dist folder.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path')
 var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
@@ -8,6 +9,11 @@ module.exports = {
     publicPath: '/dist/',
     filename: 'build.js'
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Stock Trader'
+    })
+  ],
   module: {
     rules: [
       {
